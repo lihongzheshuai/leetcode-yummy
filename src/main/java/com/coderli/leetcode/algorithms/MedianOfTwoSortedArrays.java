a0d0618a5c4990f5a158e25a275310b4f1a7df33
wushikezuo@gmail.com <wushikezuo@gmail.com>
2015-09-08 22:58:28 +0800

    leetcode median temp

--- src/main/java/com/coderli/leetcode/algorithms/MedianOfTwoSortedArrays.java
+++ src/main/java/com/coderli/leetcode/algorithms/MedianOfTwoSortedArrays.java
@@ -9,10 +9,44 @@ package com.coderli.leetcode.algorithms;
  */
 public class MedianOfTwoSortedArrays {
 
+	public static void main(String[] args) {
+		Solution solution = new MedianOfTwoSortedArrays().new Solution();
+		int[] arrayOne = new int[]{1, 3, 4, 5, 6};
+		int[] arrayTwo = new int[]{3, 4, 5, 6, 7, 8};
+		System.out.println(solution.findMedianSortedArrays(arrayOne, arrayTwo));
+	}
 
-    public class Solution {
-        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            return 0;
-        }
-    }
+
+	public class Solution {
+
+		public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+			int lengthOne = nums1.length;
+			int lengthTwo = nums2.length;
+			// 处理一个数组长度为0的边界情况
+			if (lengthOne == 0) {
+				if (lengthTwo % 2 == 1) {
+					int midIndex = lengthTwo / 2;
+					return nums2[midIndex];
+				} else {
+					int midOne = lengthTwo / 2 - 1;
+					int midTwo = lengthTwo / 2;
+					return (nums2[midOne] + nums2[midTwo]) / 2;
+				}
+			}
+			if (lengthTwo == 0) {
+				if (lengthOne % 2 == 1) {
+					int midIndex = lengthOne / 2;
+					return nums1[midIndex];
+				} else {
+					int midOne = lengthOne / 2 - 1;
+					int midTwo = lengthOne / 2;
+					return (nums1[midOne] + nums1[midTwo]) / 2;
+				}
+			}
+			int totalLength = lengthOne + lengthTwo;
+
+
+			return 0;
+		}
+	}
 }
