d7485acbd72a0178c37ec2a75b5eee966fa0e07a
wushikezuo@gmail.com <wushikezuo@gmail.com>
2017-10-31 23:57:33 +0800

    maximum subarray draft

---
+++ src/main/java/com/coderli/leetcode/algorithms/easy/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package com.coderli.leetcode.algorithms.easy;
+
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * <p>
+ * For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ * the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ *
+ * @author OneCoder 2017-10-31 23:41
+ */
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        System.out.println(maximumSubarray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
+
+}
