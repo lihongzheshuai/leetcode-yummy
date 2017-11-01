952e1acf4d48dfc2cb610dddbf5e6527c466d1ac
wushikezuo@gmail.com <wushikezuo@gmail.com>
2017-11-01 23:47:21 +0800

    maximum subarray

--- src/main/java/com/coderli/leetcode/algorithms/easy/MaximumSubarray.java
+++ src/main/java/com/coderli/leetcode/algorithms/easy/MaximumSubarray.java
@@ -13,10 +13,24 @@ public class MaximumSubarray {
     public static void main(String[] args) {
         MaximumSubarray maximumSubarray = new MaximumSubarray();
         System.out.println(maximumSubarray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(maximumSubarray.maxSubArray(new int[]{-2, -1}));
+        System.out.println(maximumSubarray.maxSubArray(new int[]{1, -3, 2}));
+        System.out.println(maximumSubarray.maxSubArray(new int[]{1, -1, -2, 2}));
     }
 
     public int maxSubArray(int[] nums) {
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        int maxResult = nums[0];
+        int tempSum = maxResult;
+        for (int i = 1; i < nums.length; i++) {
+            tempSum = tempSum + nums[i];
+            tempSum = Math.max(tempSum, nums[i]);
+            maxResult = Math.max(tempSum, maxResult);
+
+        }
+        return maxResult;
     }
 
 }
