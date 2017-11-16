875b0e30a6ccf2a2c16f7bda4eff50e98af76223
OneCoder <wushikezuo@gmail.com>
2017-11-16 13:38:28 +0800

    BinaryTreeLevelOrderTraversal2 draft

---
+++ src/main/java/com/coderli/leetcode/algorithms/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,27 @@
+package com.coderli.leetcode.algorithms.easy;
+
+/**
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ *
+ * @author li.hzh 2017-11-16 11:51
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
