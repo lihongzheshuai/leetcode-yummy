03b7da40c5ef61ca207c78066e512a0d6a114fcf
wushikezuo@gmail.com <wushikezuo@gmail.com>
2017-11-22 00:07:50 +0800

    ConvertSortedArrayToBinarySearchTree Draft

--- src/main/java/com/coderli/leetcode/algorithms/easy/ConvertSortedArrayToBinarySearchTree.java
+++ src/main/java/com/coderli/leetcode/algorithms/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -8,10 +8,24 @@ package com.coderli.leetcode.algorithms.easy;
 public class ConvertSortedArrayToBinarySearchTree {
 
     public static void main(String[] args) {
-
+        ConvertSortedArrayToBinarySearchTree convert = new ConvertSortedArrayToBinarySearchTree();
+        printTreeMidOrder(convert.sortedArrayToBST(new int[]{2, 3, 4, 5, 7, 8, 9}));
     }
 
     public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int left = 0;
+        int right = nums.length;
+        int mid = left + (right - left) / 2;
+        TreeNode tree = new TreeNode(mid);
+        tree.left = sortedArrayToBST(left, mid, nums, tree);
+        tree.right = sortedArrayToBST(mid + 1, right, nums, tree);
+        return tree;
+    }
+
+    private TreeNode sortedArrayToBST(int from, int to, int[] nums, TreeNode parent) {
         return null;
     }
 
@@ -23,5 +37,19 @@ public class ConvertSortedArrayToBinarySearchTree {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    private static void printTreeMidOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (node.left != null) {
+            printTreeMidOrder(node.left);
+        }
+        System.out.print(node.val + " ");
+        if (node.right != null) {
+            printTreeMidOrder(node.right);
+        }
+
     }
 }
