f65d9bdc2b35f9f5805cd6d7d1e9263f874a597f
wushikezuo@gmail.com <wushikezuo@gmail.com>
2018-01-23 13:23:53 +0800

    best match

--- src/main/python/others/best_match.py
+++ src/main/python/others/best_match.py
@@ -2,6 +2,8 @@ from decimal import Decimal
 
 # 保留几位小数
 quantize = Decimal("0.00")
+
+
 def do_match(assets, funds, relationsa2f, relationsf2a, max_single_pay_ration, if_sort_asset):
     # 先检查关联关系
     if relationsa2f is None or len(relationsa2f) == 0:
@@ -16,46 +18,62 @@ def do_match(assets, funds, relationsa2f, relationsf2a, max_single_pay_ration, i
     # 保存实时资金方余额
     funds_remaining = funds
     # 保存实时资产方需求余量
-    assets_reaming = assets
-    # 遍历资产方（资金需求方），计算份数和每份资金数
-    for index in range(len(assets)):
-        # 需求资金
-        a_val = assets_reaming[index]
-        # 关联资金数组
-        a_relations = relationsa2f[index]
-        # 份数
-        share_count = len(a_relations)
-        # 每份资金数
-        if share_count != 0 and a_val > 0:
-            asset_per_demand[index] = (Decimal(a_val) / Decimal(share_count)).quantize(quantize)
-        else:
-            asset_per_demand[index] = Decimal(0)
-        # 遍历资金方，计算被需求金额，并与自身资金量比较
-        for fund_index in a_relations:
-            # 实时资金量
-            cur_fund_val = funds_remaining[fund_index]
-            # 当前资金方单笔可支付最大金额
-            cur_max_count = (cur_fund_val * get_real_ration(max_single_pay_ration, relationsf2a[index])).quantize(quantize)
-            # 定义Map保存付款映射关系
-            cur_pay_map = {} if funds_pay[fund_index] is None else funds_pay[fund_index]
-            cur_pay_count = 0
-            if asset_per_demand[index] <= cur_max_count and asset_per_demand[index] <= cur_fund_val:
-                # 既小于余额又小于单笔自大限额，正常支付需求
-                cur_pay_count = asset_per_demand[index]
-            elif asset_per_demand[index] > cur_max_count and asset_per_demand[index] <= cur_fund_val:
-                # 余额充足，但是大于大笔最大限额，按限额支付
-                cur_pay_count = cur_max_count
+    assets_reaming = {}
+    for idx in range(len(assets)):
+        assets_reaming[idx] = assets[idx]
+    # 如果按照关联资金方升序排列
+    sorted_assets = sort_assets(assets_reaming, relationsa2f, if_sort_asset)
+    need_continue = True
+    # 可调参数
+    iter_count = 10
+    while iter_count > 0:
+        iter_count -= 1
+        need_continue = False
+        # 遍历资产方（资金需求方），计算份数和每份资金数
+        for index in sorted_assets.keys():
+            # 需求资金
+            a_val = assets_reaming[index]
+            if a_val <= 0:
+                continue
+            # 关联资金数组
+            a_relations = relationsa2f[index]
+            # 份数
+            share_count = len(a_relations)
+            # 每份资金数
+            if share_count != 0 and a_val > 0:
+                asset_per_demand[index] = (Decimal(a_val) / Decimal(share_count)).quantize(quantize)
             else:
-                # 小于限额，但是余额不足了。全部支付
-                cur_pay_count = cur_fund_val
-            # 保存支付结果到Map中
-            if cur_pay_map.get(index) is None:
-                cur_pay_map[index] = cur_pay_count
-            else:
-                cur_pay_map[index] += cur_pay_count
-            assets_reaming[index] -= cur_pay_count
-            funds_remaining[fund_index] -= cur_pay_count
-            funds_pay[fund_index] = cur_pay_map
+                asset_per_demand[index] = Decimal(0)
+            # 遍历资金方，计算被需求金额，并与自身资金量比较
+            for fund_index in a_relations:
+                # 实时资金量
+                cur_fund_val = funds_remaining[fund_index]
+                if cur_fund_val <= 0:
+                    continue
+                need_continue = True
+                # 当前资金方单笔可支付最大金额
+                cur_max_count = (cur_fund_val * get_real_ration(max_single_pay_ration, relationsf2a[index])).quantize(
+                    quantize)
+                # 定义Map保存付款映射关系
+                cur_pay_map = {} if funds_pay[fund_index] is None else funds_pay[fund_index]
+                cur_pay_count = 0
+                if asset_per_demand[index] <= cur_max_count and asset_per_demand[index] <= cur_fund_val:
+                    # 既小于余额又小于单笔自大限额，正常支付需求
+                    cur_pay_count = asset_per_demand[index]
+                elif asset_per_demand[index] > cur_max_count and asset_per_demand[index] <= cur_fund_val:
+                    # 余额充足，但是大于大笔最大限额，按限额支付
+                    cur_pay_count = cur_max_count
+                else:
+                    # 小于限额，但是余额不足了。全部支付
+                    cur_pay_count = cur_fund_val
+                # 保存支付结果到Map中
+                if cur_pay_map.get(index) is None:
+                    cur_pay_map[index] = cur_pay_count
+                else:
+                    cur_pay_map[index] += cur_pay_count
+                assets_reaming[index] -= cur_pay_count
+                funds_remaining[fund_index] -= cur_pay_count
+                funds_pay[fund_index] = cur_pay_map
     result["资金剩余"] = funds_remaining
     result["资产需求未满足量"] = assets_reaming
     result["资金方支付"] = funds_pay
@@ -63,23 +81,23 @@ def do_match(assets, funds, relationsa2f, relationsf2a, max_single_pay_ration, i
     # print("未进行二次分配时的结果")
     # print(result)
     # 开始进行二次分配, 二次分配采用一下最大满足策略，忽略最大出资比例
-    for index in range(len(assets_reaming)):
-        for fund_index in relationsa2f[index]:
-            a_demand_remaind_count = assets_reaming[index]
-            if a_demand_remaind_count <= 0:
-                break
-            cur_fund_remaining_count = funds_remaining[fund_index]
-            cur_max_fund_pay = Decimal(cur_fund_remaining_count * get_real_ration(max_single_pay_ration, relationsf2a)).quantize(quantize)
-            if cur_fund_remaining_count <= 0:
-                continue
-            else:
-                match_count = Decimal(a_demand_remaind_count if a_demand_remaind_count <=  cur_max_fund_pay else cur_max_fund_pay)
-            funds_remaining[fund_index] -= match_count
-            assets_reaming[index] -= match_count
-            if dict(funds_pay[fund_index]).get(index) is None:
-                funds_pay[fund_index][index] = match_count
-            else:
-                funds_pay[fund_index][index] += match_count
+    # for index in range(len(assets_reaming)):
+    #     for fund_index in relationsa2f[index]:
+    #         a_demand_remaind_count = assets_reaming[index]
+    #         if a_demand_remaind_count <= 0:
+    #             break
+    #         cur_fund_remaining_count = funds_remaining[fund_index]
+    #         cur_max_fund_pay = Decimal(cur_fund_remaining_count * get_real_ration(max_single_pay_ration, relationsf2a)).quantize(quantize)
+    #         if cur_fund_remaining_count <= 0:
+    #             continue
+    #         else:
+    #             match_count = Decimal(a_demand_remaind_count if a_demand_remaind_count <=  cur_max_fund_pay else cur_max_fund_pay)
+    #         funds_remaining[fund_index] -= match_count
+    #         assets_reaming[index] -= match_count
+    #         if dict(funds_pay[fund_index]).get(index) is None:
+    #             funds_pay[fund_index][index] = match_count
+    #         else:
+    #             funds_pay[fund_index][index] += match_count
     # 二次分配后
     return result
 
@@ -111,11 +129,26 @@ def do_test(ratio, is_sort):
     print(detail_result)
     return do_match(assets, funds, relationsa2f, relationsf2a, ratio, is_sort)
 
+
+# TODO 排序资产方
+import operator
+
+
+def sort_assets(assets, relations_a2f, if_sort):
+    sorted_assets_map = rela_count_map = {}
+    for idx in range(len(relations_a2f)):
+        rela_count_map[idx] = len(relations_a2f[idx])
+    sorted_assets_map = rela_count_map
+    if if_sort:
+        sorted_assets_map = dict(sorted(rela_count_map.items(), key=operator.itemgetter(1)))
+    return sorted_assets_map
+
+
 # auto
 # do_test(-1, False)
 for i in range(1, 11):
     ratio = Decimal(0.1 * i).quantize(Decimal("0.0"))
-    do_test(ratio, False)
+    do_test(ratio, True)
 
 # for i in range(1, 11):
 #     ratio = Decimal(0.1 * i).quantize(Decimal("0.0"))
