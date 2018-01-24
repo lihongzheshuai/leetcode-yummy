37c8bc48e74a2d8f78849114ebc77b12e527c187
wushikezuo@gmail.com <wushikezuo@gmail.com>
2018-01-24 21:32:48 +0800

    best match v0.3.1 with recommend

---
+++ src/main/python/others/best_match_test.py
@@ -0,0 +1,44 @@
+import best_match
+from decimal import Decimal
+
+is_print_detail = True
+# 资产方定义。值代表拥有的资产数
+assets = [Decimal(14), Decimal(8), Decimal(18)]
+# 资金方定义，值代表拥有的资金数
+funds = [Decimal(10), Decimal(10), Decimal(20)]
+# 关联关系定义，从资产 -> 资金
+relationsa2f = [[0, 2], [0, 1, 2], [2]]
+# 关联关系定义 从资金 -> 资产，a2f和f2a关系必须一致不能矛盾
+relationsf2a = [[0, 1], [1], [0, 1, 2]]
+match_result = best_match.do_run(assets, funds, relationsa2f, relationsf2a, is_print_detail)
+print("系统推荐方案")
+print(match_result)
+print()
+
+is_print_detail = True
+# 资产方定义。值代表拥有的资产数
+assets = [Decimal(14), Decimal(8), Decimal(18)]
+# 资金方定义，值代表拥有的资金数
+funds = [Decimal(10), Decimal(10), Decimal(20)]
+# 关联关系定义，从资产 -> 资金
+relationsa2f = [[0, 2], [0, 1, 2], [2]]
+# 关联关系定义 从资金 -> 资产，a2f和f2a关系必须一致不能矛盾
+relationsf2a = [[0, 1], [1], [0, 1, 2]]
+match_result = best_match.do_run(assets, funds, relationsa2f, relationsf2a, is_print_detail)
+print("系统推荐方案")
+print(match_result)
+print()
+
+is_print_detail = True
+# 资产方定义。值代表拥有的资产数
+assets = [Decimal(120), Decimal(8), Decimal(18)]
+# 资金方定义，值代表拥有的资金数
+funds = [Decimal(10), Decimal(10), Decimal(20)]
+# 关联关系定义，从资产 -> 资金
+relationsa2f = [[0, 2], [0, 1, 2], [1, 2]]
+# 关联关系定义 从资金 -> 资产，a2f和f2a关系必须一致不能矛盾
+relationsf2a = [[0, 1], [1, 2], [0, 1, 2]]
+match_result = best_match.do_run(assets, funds, relationsa2f, relationsf2a, is_print_detail)
+print("系统推荐方案")
+print(match_result)
+print()
